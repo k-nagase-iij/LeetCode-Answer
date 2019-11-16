993226d1161ba6847e68791f255919b97c281f83
Kohei Nagase <k-nagase@iij.ad.jp>
2019-11-16 22:23:02 +0900

    Add reverseInteger

--- ReverseInteger/reverseInteger.go
+++ ReverseInteger/reverseInteger.go
@@ -1,5 +1,41 @@
 package main
 
-func reverse(x int) int {
+import (
+	"fmt"
+	"math"
+)
 
+func reverse(x int) int {
+	neg := false
+	var answer int
+
+	if x < 0 {
+		x = x * -1
+		neg = true
+	}
+
+	for x != 0 {
+		n := x % 10
+		answer = n + answer*10
+		if answer > math.MaxInt32 || answer < math.MinInt32 {
+			return 0
+		}
+		x = x / 10
+	}
+
+	if neg == true {
+		answer *= -1
+		neg = false
+	}
+
+	return answer
+}
+
+func main() {
+	x := 123
+	fmt.Printf("%d -> %d\n", x, reverse(x))
+	x = -123
+	fmt.Printf("%d -> %d\n", x, reverse(x))
+	x = 1534236469
+	fmt.Printf("%d -> %d\n", x, reverse(x))
 }
