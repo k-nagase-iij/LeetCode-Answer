5dab9b9c08462b67e1493282598c4d54a1b58bb9
Kohei Nagase <k-nagase@iij.ad.jp>
2019-11-26 20:16:59 +0900

    Add a problem

--- LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.go
+++ LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.go
@@ -1,7 +1,22 @@
 package LongestSubstringWithoutRepeatingCharacters
 
 func lengthOfLongestSubstring(s string) int {
-    if s == nil || len(s) == 1 {
-		return s
+	if s == "" {
+		return 0
 	}
+	m, max, start := make(map[rune]int), 0, 0
+	for i, c := range s {
+		_, ok := m[c]
+		if ok == true && start <= m[c] {
+			if i-start > max {
+				max = i - start
+			}
+			start = m[c] + 1
+		}
+		m[c] = i
+	}
+	if len(s)-start > max {
+		max = len(s) - start
+	}
+	return max
 }
