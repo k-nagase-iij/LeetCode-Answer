9d4cf3d2ad74704ad50a2c7cea16adcf9fce9166
Kohei Nagase <k-nagase@iij.ad.jp>
2019-12-05 19:29:00 +0900

    Add a problem

---
+++ ZigZagConversion/ZigZagConversion.go
@@ -0,0 +1,33 @@
+package ZigZagConversion
+
+import "bytes"
+
+func convert(s string, numRows int) string {
+	if numRows == 1 || len(s) <= numRows {
+		return s
+	}
+
+	res := bytes.Buffer{}
+	p := 2*numRows - 2
+
+	for i := 0; i < len(s); i += p {
+		res.WriteByte(s[i])
+	}
+
+	for r := 1; r <= numRows-2; r++ {
+		res.WriteByte(s[r])
+
+		for k := p; k-r < len(s); k += p {
+			res.WriteByte(s[k-r])
+			if k+r < len(s) {
+				res.WriteByte(s[k+r])
+			}
+		}
+	}
+
+	for i := numRows - 1; i < len(s); i += p {
+		res.WriteByte(s[i])
+	}
+
+	return res.String()
+}
